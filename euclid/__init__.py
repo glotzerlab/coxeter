9c97eabaddf59f554d11b42728ee41182c69fe25
Bryan VanSaders <bvansade@umich.edu>
2016-08-30 23:43:06 -0400

    Added in my own shape tools and python package scaffold

---
+++ euclid/__init__.py
@@ -0,0 +1,4 @@
+import numpy as np
+from scipy.spatial import ConvexHull, Delaunay
+
+from euclid.shapes import *
---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name = 'euclid',
+        version = '0.1',
+        description = 'shape tools',
+        url = 'https://bvansade@bitbucket.org/glotzer/euclid.git',
+        author = 'Bryan VanSaders',
+        author_email = 'bvansade@umich.edu',
+        license = 'None',
+        packages = ['euclid'],
+        zip_safe = False)
