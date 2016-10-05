0b3cea13671620b7e51a2c1f38b7ef5b52b51409
Bryan VanSaders <bvansade@umich.edu>
2016-10-04 21:21:30 -0400

    Added the start of a shape class

--- euclid/shapes.py
+++ euclid/shapes.py
@@ -1,5 +1,57 @@
 # For calling up commonly used shapes
 from . import np
+from . import ConvexHull
+from . import utils
+
+# Base class for shapes. It really doesn't work for 2D stuff yet
+class Shape(object):
+
+    # The IDstr is a string identifier. Can be a name or
+    # a numerical designation. 'Content' is a dimension
+    # agnostic way to say 'volume' or 'area'
+    def __init__(self, vertices, radius=0, content=None):
+        self.ndims = vertices.shape[1]
+        self.radius = radius
+
+        if self.radius > 0:
+            self.is_sphero = True
+        else:
+            self.is_sphero = False
+
+
+        # Change the volume of the shape
+        if content is not None:
+            self.content = self.normalizeContent(self, vertices, content)
+        else:
+            self.content = self.git Content(self, vertices)
+        else:
+            self.verts = np.asarray(vertices)
+            if self.is_sphero:
+                self.content = utils.spheropolyhedra_volume(vertices, R=radius)
+            else:
+                self.content = ConvexHull(vertices).volume()
+
+
+        self.hull = ConvexHull(vertices)
+
+    # A function for normalizing the content of a shape
+    def normalizeContent(self, vertices, content):
+        vol = self.getContent(self, vertices)
+        self.content = content
+        self.vertices = vertices/np.power(vol,1/self.ndims)
+
+        return None
+
+
+    # a function for finding the content of a shape
+    def getContent(self, vertices):
+        if self.is_sphero:
+            vol = utils.spheropolyhedra_volume(vertices, R=radius)
+        else:
+            vol = ConvexHull(vertices).volume()
+
+        return vol
+
 
 # Truncation ranges from 0 (octahedra) to 2/3 (truncated octahedra) to 1 (cuboctahedra)
 def octahedron(side_length = 1, truncation = 0):
