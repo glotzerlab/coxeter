d11488b1518cd16e832616b60043288c6f145690
Vyas Ramasubramani <vramasub@umich.edu>
2019-12-02 14:40:15 -0800

    Add check for convexity of the spheropolygon.

--- euclid/shape_classes/spheropolygon.py
+++ euclid/shape_classes/spheropolygon.py
@@ -3,19 +3,31 @@ import numpy as np
 from .polygon import Polygon
 
 
-class Spheropolygon(object):
-    def __init__(self, vertices, radius, normal=None):
-        """A simple (i.e. non-self-overlapping) spheropolygon.
+def _check_convex(vertices):
+    """Check if a set of vertices defines a convex polygon.
 
-        The polygon may be embedded in 3-dimensions, in which case the normal
-        vector determines which way is "up". Note that the precise normal
-        vector is not important to the winding number because the winding
-        number is invariant to projection, all we need to know is the sign of
-        each component of the normal vector.
+    This algorithm assumes that the vertices define a non-intersecting polygon.
+    The vertices must be consecutively ordered. Raises a ValueError if the
+    vertices form a nonconvex polygon.
+    """
+    shifted_forward = np.roll(vertices, shift=1, axis=0)
+    shifted_backward = np.roll(vertices, shift=-1, axis=0)
+
+    cross = np.cross(shifted_backward - vertices, vertices - shifted_forward)
+
+    if len(np.unique(np.sign(cross[:, 2]))) > 1:
+        raise ValueError("The vertices do not define a convex polygon.")
+
+
+class ConvexSpheropolygon(object):
+    def __init__(self, vertices, radius, normal=None):
+        """A convex spheropolygon.
 
         Args:
             vertices (:math:`(N, 3)` or :math:`(N, 2)` :class:`numpy.ndarray`):
                 The vertices of the polygon.
+            radius (float):
+                The rounding radius of the spheropolygon.
             normal (sequence of length 3 or None):
                 The normal vector to the polygon. If :code:`None`, the normal
                 is computed by taking the cross product of the vectors formed
@@ -24,11 +36,11 @@ class Spheropolygon(object):
                 arbitrary choice may not preserve the orientation of the
                 provided vertices, users may provide a normal instead
                 (Default value: None).
-
         """
         if radius < 0:
             raise ValueError("The radius must be positive.")
         self.polygon = Polygon(vertices, normal)
+        _check_convex(vertices)
         self._radius = radius
 
     @property
