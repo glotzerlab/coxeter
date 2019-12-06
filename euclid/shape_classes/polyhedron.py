Fix missing normalization of normal vector that broke volume calculations. Also make some minor optimizations and simplifications.

euclid/shape_classes/polyhedron.py
@@ -5,6 +5,8 @@ from scipy.sparse.csgraph import connected_components
 
 
 def _facet_to_edges(facet, reverse=False):
+    """Convert a facet (a sequence of vertices) into a sequence of edges
+    (tuples)."""
     shift = 1 if reverse else -1
     return list(zip(*np.stack((facet, np.roll(facet, shift)))))
 
@@ -46,7 +48,8 @@ class Polyhedron(object):
             normal = np.cross(
                 self.vertices[facet[2]] - self.vertices[facet[1]],
                 self.vertices[facet[0]] - self.vertices[facet[1]])
-            self._equations[i, :3] = normal / np.linalg.norm(normal)
+            normal /= np.linalg.norm(normal)
+            self._equations[i, :3] = normal
             self._equations[i, 3] = normal.dot(self.vertices[facet[0]])
 
     def _find_neighbors(self):
@@ -115,7 +118,7 @@ class Polyhedron(object):
         """Ensure that all facets are ordered such that the normals are
         counterclockwise and point outwards.
 
-        This algorithm proceeds in three steps. First, it ensures that each
+        This algorithm proceeds in four steps. First, it ensures that each
         facet is ordered in either clockwise or counterclockwise order such
         that edges can be found from the sequence of the vertices in each
         facet. Next, it calls the neighbor finding routine to establish with
@@ -132,16 +135,15 @@ class Polyhedron(object):
                 np.where(np.all(self.vertices == vertex, axis=1))[0][0]
                 for vertex in Polygon(self.vertices[facet]).vertices
             ])
-
         self._find_neighbors()
 
         # The initial facet sets the order of the others.
         visited_facets = []
         remaining_facets = [0]
         while len(remaining_facets):
-            current_facet = remaining_facets[0]
+            current_facet = remaining_facets[-1]
             visited_facets.append(current_facet)
-            remaining_facets.remove(current_facet)
+            remaining_facets.pop()
 
             # Search for common edges between pairs of facets, then check the
             # ordering of the edge to determine relative facet orientation.
@@ -183,8 +185,6 @@ class Polyhedron(object):
     @property
     def volume(self):
         """Get or set the polyhedron's volume (setting rescales vertices)."""
-        # Arbitrary choice, use the first vertex in the face to compute the
-        # distance to the plane.
         ds = self._equations[:, 3]
         return np.sum(ds*self.get_facet_area())/3
 

tests/test_polyhedron.py
@@ -2,8 +2,7 @@ import pytest
 import numpy as np
 from euclid.shape_classes.polyhedron import Polyhedron
 from scipy.spatial import ConvexHull
-from euclid.polyhedron import ConvexPolyhedron
-from hypothesis import given, example, assume
+from hypothesis import given
 from hypothesis.strategies import floats
 from hypothesis.extra.numpy import arrays
 
@@ -101,35 +100,16 @@ def test_facet_alignment(cube):
         assert any([str_facet in ref for ref in reference_facets])
 
 
-# @given(arrays(np.float64, (5, 3), floats(-10, 10, width=64), unique=True))
-# @example(points=np.array([[1.        , 1.0005    , 1.0015    ],
-                          # [1.0025    , 1.0045    , 1.0055    ],
-                          # [1.0065    , 1.00750053, 1.12304688],
-                          # [1.15429688, 1.15625   , 1.16210938],
-                          # [1.00390625, 1.20373154, 1.24704742]]))
-# @example(points=np.array([[1.0351543 , 1.        , 1.0005    ],
-                       # [1.0015    , 1.0025    , 1.0035    ],
-                       # [1.0045    , 1.0055    , 1.00683504],
-                       # [1.00781354, 1.00859256, 1.08362384],
-                       # [1.00976737, 1.24999805, 1.25050087]]))
-# @example(points=np.array([[0.00000000e+00, 5.00000000e-04, 1.50000000e-03],
-                          # [2.50000000e-03, 3.50000000e-03, 4.50000000e-03],
-                          # [5.50000000e-03, 1.95258026e-02, 1.24999455e+00],
-                          # [1.25050000e+00, 9.76017761e-03, 3.90619553e-01],
-                          # [8.12499794e+00, 8.12692097e+00, 8.33251408e+00]]))
-# @example(points=np.array([[5.00000000e-04, 1.50000000e-03, 2.50000000e-03],
-                          # [3.50000000e-03, 4.50000000e-03, 5.50000000e-03],
-                          # [6.50000000e-03, 7.50000000e-03, 6.13777472e-01],
-                          # [8.50000000e-03, 8.08292465e+00, 0.00000000e+00],
-                          # [1.21515936e+00, 9.50000000e-03, 1.05000000e-02]]))
-# def test_convex_volume(points):
-    # """Check the volumes of various convex sets."""
-    # assume(points.size == np.unique(np.round(points, 3)).size)
-    # hull = ConvexHull(points)
-    # assume(hull.volume > 1)
-    # verts = points[hull.vertices]
-    # poly = Polyhedron(verts)
-    # assert np.isclose(hull.volume, poly.volume)
+@given(arrays(np.float64, (5, 3), floats(-10, 10, width=64), unique=True))
+def test_convex_volume(points):
+    """Check the volumes of various convex sets."""
+    hull = ConvexHull(points)
+    verts = points[hull.vertices]
+    poly = Polyhedron(verts)
+    poly.merge_facets()
+    poly.sort_facets()
+
+    assert np.isclose(hull.volume, poly.volume)
 
 
 @given(arrays(np.float64, (5, 3), floats(1, 5, width=64), unique=True))
@@ -138,4 +118,6 @@ def test_convex_surface_area(points):
     hull = ConvexHull(points)
     verts = points[hull.vertices]
     poly = Polyhedron(verts)
+    poly.merge_facets()
+    poly.sort_facets()
     assert np.isclose(hull.area, poly.surface_area)