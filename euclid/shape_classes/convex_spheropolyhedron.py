b05e1674eeb797225fe11c0ba1b650b367a048f1
Vyas Ramasubramani <vramasub@umich.edu>
2019-12-10 20:00:19 -0500

    Add desired properties to convex spheropolyhedron.

--- euclid/shape_classes/convex_spheropolyhedron.py
+++ euclid/shape_classes/convex_spheropolyhedron.py
@@ -7,7 +7,9 @@ class ConvexSpheropolyhedron(object):
         """A convex spheropolyhedron.
 
         A convex spheropolyhedron is defined as a convex polyhedron plus a
-        rounding radius.
+        rounding radius. All properties of the underlying polyhedron (the
+        vertices, the facets and their neighbors, etc) can be accessed directly
+        through :attr:`.polyhedron`.
 
         Args:
             vertices (:math:`(N, 3)` :class:`numpy.ndarray`):
@@ -19,11 +21,23 @@ class ConvexSpheropolyhedron(object):
         self._radius = radius
 
     @property
+    def polyhedron(self):
+        """:class:`~euclid.shape_classes.convex_polyhedron.ConvexPolyhedron`:
+        The underlying polyhedron."""
+        return self._polyhedron
+
+    @property
     def volume(self):
         """float: The volume."""
-        return self._polyhedron.volume + (4/3)*np.pi*self._radius**3
+        # WRONG:Need to add the cylinder volumes too.
+        return self.polyhedron.volume + (4/3)*np.pi*self._radius**3
 
     @property
     def radius(self):
         """float: The rounding radius."""
         return self._radius
+
+    @property
+    def surface_area(self):
+        """float: The surface area."""
+        pass
