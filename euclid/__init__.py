abf0ee302e8bf640301be2c5d9558ae70652333b
Vyas Ramasubramani <vramasub@umich.edu>
2020-01-20 01:22:22 -0500

    Remove imports from __init__

--- euclid/__init__.py
+++ euclid/__init__.py
@@ -2,19 +2,14 @@ from . import shapes
 from . import damasceno
 from . import symmetry
 from . import ft
-from . import polygon
-from . import polyhedron
 from . import shape_classes
 from . import common_shapes
 
 __all__ = [
     'shapes',
-    'utils',
     'damasceno',
     'symmetry',
     'ft',
-    'polygon',
-    'polyhedron',
     'shape_classes',
     'common_shapes',
 ]
