274d67987d4b7e6ef5fdac8e32387dfecc5a6632
Bradley Dice <bdice@bradleydice.com>
2020-01-22 09:18:10 -0500

    Add function to make sphero-cube.

--- tests/test_spheropolyhedron.py
+++ tests/test_spheropolyhedron.py
@@ -1,13 +1,13 @@
 import numpy as np
-from euclid.shape_classes.convex_spheropolyhedron import ConvexSpheropolyhedron
+from conftest import make_sphero_cube
 
 from hypothesis import given
 from hypothesis.strategies import floats
 
 
 @given(radius=floats(0.1, 1))
-def test_volume(radius, cube_points):
-    sphero_cube = ConvexSpheropolyhedron(cube_points, radius)
+def test_volume(radius):
+    sphero_cube = make_sphero_cube(radius=radius)
     V_cube = 1
     V_sphere = (4/3)*np.pi*radius**3
     V_cyl = 12*(np.pi*radius**2)/4
@@ -17,28 +17,28 @@ def test_volume(radius, cube_points):
 def test_volume_polyhedron(convex_cube, cube_points):
     """Ensure that the base case of zero radius gives the same result as a
     polyhedron."""
-    sphero_cube = ConvexSpheropolyhedron(cube_points, 0)
+    sphero_cube = make_sphero_cube(radius=0)
     assert sphero_cube.volume == convex_cube.volume
 
 
 @given(radius=floats(0.1, 1))
-def test_surface_area(radius, cube_points):
-    sphero_cube = ConvexSpheropolyhedron(cube_points, radius)
+def test_surface_area(radius):
+    sphero_cube = make_sphero_cube(radius=radius)
     S_cube = 6
     S_sphere = 4*np.pi*radius**2
     S_cyl = 12*(2*np.pi*radius)/4
     assert np.isclose(sphero_cube.surface_area, S_cube + S_sphere + S_cyl)
 
 
-def test_surface_area_polyhedron(convex_cube, cube_points):
+def test_surface_area_polyhedron(convex_cube):
     """Ensure that the base case of zero radius gives the same result as a
     polyhedron."""
-    sphero_cube = ConvexSpheropolyhedron(cube_points, 0)
+    sphero_cube = make_sphero_cube(radius=0)
     assert sphero_cube.surface_area == convex_cube.surface_area
 
 
-def test_inside_boundaries(cube_points):
-    sphero_cube = ConvexSpheropolyhedron(cube_points, 1)
+def test_inside_boundaries():
+    sphero_cube = make_sphero_cube(radius=1)
 
     points_inside = [[0, 0, 0], [1, 1, 1], [-0.01, -0.01, -0.01],
                      [2, 0.5, 0.5], [2, 1, 0.5], [0.5, -0.7, -0.7],
