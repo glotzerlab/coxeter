8804e29b6993e3f8bde525f625688c0eb67e5805
Bradley Dice <bdice@bradleydice.com>
2020-02-11 00:04:09 -0500

    Add eccentricity ratio test.

--- tests/test_circle.py
+++ tests/test_circle.py
@@ -1,3 +1,4 @@
+import pytest
 import numpy as np
 from euclid.shape_classes.circle import Circle
 from hypothesis import given
@@ -41,4 +42,4 @@ def test_inertia_tensor(r):
     expected = np.pi / 4 * r**4
     np.testing.assert_allclose(circle.planar_moments_inertia[:2], expected)
     np.testing.assert_allclose(circle.planar_moments_inertia[2], 0)
-    assert np.isclose(circle.polar_moment_inertia, expected*2)
+    assert circle.polar_moment_inertia == pytest.approx(expected*2)
--- tests/test_ellipse.py
+++ tests/test_ellipse.py
@@ -40,6 +40,15 @@ def test_eccentricity(a, b):
 
 
 @given(floats(0.1, 1000), floats(0.1, 1000))
+def test_eccentricity_ratio(a, k):
+    b = a * k
+    ellipse = Ellipse(a, b)
+    b, a = sorted([a, b])
+    expected = np.sqrt(1 - b**2 / a**2)
+    assert ellipse.eccentricity == pytest.approx(expected)
+
+
+@given(floats(0.1, 1000), floats(0.1, 1000))
 def test_inertia_tensor(a, b):
     ellipse = Ellipse(a, b)
     assert np.all(np.asarray(ellipse.planar_moments_inertia) >= 0)
@@ -47,4 +56,4 @@ def test_inertia_tensor(a, b):
     expected = [np.pi / 4 * a * b**3, np.pi / 4 * a**3 * b]
     np.testing.assert_allclose(ellipse.planar_moments_inertia[:2], expected)
     np.testing.assert_allclose(ellipse.planar_moments_inertia[2], 0)
-    assert np.isclose(ellipse.polar_moment_inertia, sum(expected))
+    assert ellipse.polar_moment_inertia == pytest.approx(sum(expected))
