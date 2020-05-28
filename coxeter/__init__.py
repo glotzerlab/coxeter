26448e85d4a3e7fba271331302eda1352e4093c6
Vyas Ramasubramani <vramasub@umich.edu>
2020-05-28 12:17:32 -0400

    Update init file and add citation to bib.

--- coxeter/__init__.py
+++ coxeter/__init__.py
@@ -4,6 +4,7 @@ from . import symmetry
 from . import ft
 from . import shape_classes
 from . import common_shapes
+from . import shape_families
 from .shape_getters import from_gsd_type_shapes
 
 __all__ = [
@@ -13,7 +14,8 @@ __all__ = [
     'ft',
     'shape_classes',
     'common_shapes',
-    'from_gsd_type_shapes'
+    'from_gsd_type_shapes',
+    'shape_families'
 ]
 
 __version__ = '0.2.0'
