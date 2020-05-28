337a33275c93e4f814c55c5551761d1edb8ca032
Vyas Ramasubramani <vramasub@umich.edu>
2020-05-28 14:42:32 -0400

    Write proper tests.

--- tests/test_shape_families.py
+++ tests/test_shape_families.py
@@ -11,19 +11,34 @@ def test_shape_repos():
 
 def test_shape332():
     family = Family332()
-    print(family(1, 1, 1))
+    # Octahedron (6)
+    assert len(family(1, 1, 1).vertices) == 6
+    assert len(family(1, 1, 1).faces) == 8
+    # Tetrahedron (4)
+    assert len(family(1, 1, 3).vertices) == 4
+    assert len(family(1, 1, 3).faces) == 4
+    # Tetrahedron (4)
+    assert len(family(3, 1, 1).vertices) == 4
+    assert len(family(3, 1, 1).faces) == 4
+    # Cube (8)
+    assert len(family(3, 1, 3).vertices) == 8
+    assert len(family(3, 1, 3).faces) == 6
 
 
 def test_shape432():
     family = Family432()
     # Cuboctahedron (12)
-    print(len(family(1, 2, 2).vertices))
+    assert len(family(1, 2, 2).vertices) == 12
+    assert len(family(1, 2, 2).faces) == 14
     # Octahedron (6)
-    print(len(family(2, 2, 2).vertices))
+    assert len(family(2, 2, 2).vertices) == 6
+    assert len(family(2, 2, 2).faces) == 8
     # Cube (8)
-    print(len(family(1, 2, 3).vertices))
+    assert len(family(1, 2, 3).vertices) == 8
+    assert len(family(1, 2, 3).faces) == 6
     # Rhombic Dodecahedron (14)
-    print(len(family(2, 2, 3).vertices))
+    assert len(family(2, 2, 3).vertices) == 14
+    assert len(family(2, 2, 3).faces) == 12
 
 
 def test_shape532():
@@ -31,17 +46,20 @@ def test_shape532():
     s = ((5**0.5)-1.0)/2.
     S = ((5**0.5)+1.0)/2.
     # Icosidodecahedron
-    print(len(family(1, 2, S**2).vertices))
+    assert len(family(1, 2, S**2).vertices) == 30
+    assert len(family(1, 2, S**2).faces) == 32
     # Icosahedron
-    print(len(family(1*s*np.sqrt(5), 2, S**2).vertices))
+    assert len(family(1*s*np.sqrt(5), 2, S**2).vertices) == 12
+    assert len(family(1*s*np.sqrt(5), 2, S**2).faces) == 20
     # Dodecahedron
-    print(len(family(1, 2, 3).vertices))
+    assert len(family(1, 2, 3).vertices) == 20
+    assert len(family(1, 2, 3).faces) == 12
     # Rhombic Tricontahedron
-    print(len(family(1*s*np.sqrt(5), 2, 3).vertices))
-
-    print(family(1, 2, S**2).vertices)
-    print(family(0.1, 2, S**2).vertices)
+    assert len(family(1*s*np.sqrt(5), 2, 3).vertices) == 32
+    assert len(family(1*s*np.sqrt(5), 2, 3).faces) == 30
 
 
 if __name__ == "__main__":
+    test_shape332()
     test_shape432()
+    test_shape532()
