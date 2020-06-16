c9539f91b46c02fd2ad5c88ebde877578ba55b8d
Vyas <vramasub@umich.edu>
2020-06-16 00:02:14 -0400

    Fix is_inside checks for spheres and ellipsoids not centered at the origin.

--- coxeter/shape_classes/sphere.py
+++ coxeter/shape_classes/sphere.py
@@ -87,5 +87,5 @@ class Sphere(Shape3D):
                 Boolean array indicating which points are contained in the
                 sphere.
         """
-        points = np.atleast_2d(points)
+        points = np.atleast_2d(points) - self.center
         return np.linalg.norm(points, axis=-1) <= self.radius
