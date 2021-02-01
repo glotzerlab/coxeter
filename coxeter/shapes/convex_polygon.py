Fix calculation of center for polygons to be the true centroid.

coxeter/shapes/convex_polygon.py
@@ -139,7 +139,12 @@ class ConvexPolygon(Polygon):
                 (Default value: True).
 
         """
-        verts, _ = _align_points_by_normal(self._normal, self._vertices - self.center)
+        # The centroid cannot be computed until vertices are ordered, but for
+        # convex polygons the mean of the vertices will be contained within the
+        # shape so we can sort relative to that.
+        verts, _ = _align_points_by_normal(
+            self._normal, self._vertices - np.mean(self.vertices, axis=0)
+        )
 
         # Compute the angle of each vertex, shift so that the chosen
         # reference_index has a value of zero, then move into the [0, 2pi]

coxeter/shapes/polygon.py
@@ -381,11 +381,40 @@ class Polygon(Shape2D):
     @property
     def center(self):
         """:math:`(3, )` :class:`numpy.ndarray` of float: Get or set the centroid of the shape."""  # noqa: E501
-        return np.mean(self.vertices, axis=0)
+        # return np.mean(self.vertices, axis=0)
+        return self.centroid
 
     @center.setter
     def center(self, value):
-        self._vertices += np.asarray(value) - self.center
+        self.centroid = value
+        # self._vertices += np.asarray(value) - self.centroid
+
+    @property
+    def centroid(self):
+        """:math:`(3, )` :class:`numpy.ndarray` of float: Get or set the centroid of the shape."""  # noqa: E501
+        verts, rotation = _align_points_by_normal(self.normal, self.vertices)
+        verts_shifted = np.roll(verts, shift=-1, axis=0)
+
+        delta_term = (
+            verts[:, 0] * verts_shifted[:, 1] - verts_shifted[:, 0] * verts[:, 1]
+        )
+        c_x = np.sum((verts[:, 0] + verts_shifted[:, 0]) * delta_term)
+        c_y = np.sum((verts[:, 1] + verts_shifted[:, 1]) * delta_term)
+
+        in_plane_centroid = np.array([c_x, c_y, 0]) / (6 * self.area)
+
+        # We've rotated into the plane, so the z position of all vertices
+        # should be equal. We take the average to improve numerical stablity.
+        in_plane_centroid[2] = np.mean(verts[:, 2])
+
+        # Revert the rotation into the plane.
+        centroid = rotation.T.dot(in_plane_centroid)
+
+        return centroid
+
+    @centroid.setter
+    def centroid(self, value):
+        self._vertices += np.asarray(value) - self.centroid
 
     def _triangulation(self):
         """Generate a triangulation of the polygon.

tests/test_polygon.py
@@ -97,14 +97,16 @@ def test_set_area(square):
 def test_center(square, square_points):
     """Test centering the polygon."""
     assert np.all(square.center == np.mean(square_points, axis=0))
+    assert np.all(square.centroid == np.mean(square_points, axis=0))
     square.center = [0, 0, 0]
     assert np.all(square.center == [0, 0, 0])
+    assert np.all(square.centroid == [0, 0, 0])
 
 
 def test_moment_inertia(square):
     """Test moment of inertia calculation."""
     # First test the default values.
-    square.center = (0, 0, 0)
+    square.centroid = (0, 0, 0)
     assert np.allclose(square.planar_moments_inertia, (1 / 12, 1 / 12, 0))
     assert np.isclose(square.polar_moment_inertia, 1 / 6)
 
@@ -235,7 +237,7 @@ def test_bounding_circle_radius_random_hull(points):
     circle = poly.minimal_bounding_circle
     assert circle.radius <= rmax + 1e-6
 
-    poly.center = [0, 0, 0]
+    poly.centroid = [0, 0, 0]
     circle = poly.minimal_bounding_circle
     assert circle.radius <= rmax + 1e-6
 