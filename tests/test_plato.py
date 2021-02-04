ca4eef32bddb48e85d767af98a04a651c2d53ca1
Bradley Dice <bdice@bradleydice.com>
2021-02-04 11:29:20 -0600

    Skip plato tests if missing matplotlib.

--- tests/test_plato.py
+++ tests/test_plato.py
@@ -1,5 +1,7 @@
 import tempfile
 
+import pytest
+
 import coxeter
 
 try:
@@ -13,6 +15,10 @@ except ImportError:
     MATPLOTLIB_PLATO_AVAILABLE = False
 
 
+@pytest.mark.skipif(
+    not MATPLOTLIB_PLATO_AVAILABLE,
+    reason="plato and matplotlib are required for this test.",
+)
 def test_draw_circle():
     circle = coxeter.shapes.Circle(1)
     scene = circle.to_plato_scene("matplotlib", zoom=10)
@@ -20,6 +26,10 @@ def test_draw_circle():
         scene.save(tmp.name)
 
 
+@pytest.mark.skipif(
+    not MATPLOTLIB_PLATO_AVAILABLE,
+    reason="plato and matplotlib are required for this test.",
+)
 def test_draw_polygon():
     polygon = coxeter.shapes.Polygon([[0, 0], [1, 0], [0, 1]])
     scene = polygon.to_plato_scene("matplotlib", zoom=10)
@@ -27,6 +37,10 @@ def test_draw_polygon():
         scene.save(tmp.name)
 
 
+@pytest.mark.skipif(
+    not MATPLOTLIB_PLATO_AVAILABLE,
+    reason="plato and matplotlib are required for this test.",
+)
 def test_draw_convex_polygon():
     polygon = coxeter.shapes.ConvexPolygon([[0, 0], [1, 0], [0, 1]])
     scene = polygon.to_plato_scene("matplotlib", zoom=10)
@@ -34,6 +48,10 @@ def test_draw_convex_polygon():
         scene.save(tmp.name)
 
 
+@pytest.mark.skipif(
+    not MATPLOTLIB_PLATO_AVAILABLE,
+    reason="plato and matplotlib are required for this test.",
+)
 def test_draw_spheropolygon():
     spheropolygon = coxeter.shapes.ConvexSpheropolygon([[0, 0], [1, 0], [0, 1]], 0.3)
     scene = spheropolygon.to_plato_scene("matplotlib", zoom=10)
