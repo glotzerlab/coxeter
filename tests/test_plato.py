da037a721d058471e5dfeea1b5a6931a079ba58e
Bradley Dice <bdice@bradleydice.com>
2021-02-04 11:26:54 -0600

    Add plato tests.

---
+++ tests/test_plato.py
@@ -0,0 +1,41 @@
+import tempfile
+
+import coxeter
+
+try:
+    import matplotlib
+
+    matplotlib.use("Agg")
+    import plato.draw.matplotlib  # noqa: F401
+
+    MATPLOTLIB_PLATO_AVAILABLE = True
+except ImportError:
+    MATPLOTLIB_PLATO_AVAILABLE = False
+
+
+def test_draw_circle():
+    circle = coxeter.shapes.Circle(1)
+    scene = circle.to_plato_scene("matplotlib", zoom=10)
+    with tempfile.NamedTemporaryFile() as tmp:
+        scene.save(tmp.name)
+
+
+def test_draw_polygon():
+    polygon = coxeter.shapes.Polygon([[0, 0], [1, 0], [0, 1]])
+    scene = polygon.to_plato_scene("matplotlib", zoom=10)
+    with tempfile.NamedTemporaryFile() as tmp:
+        scene.save(tmp.name)
+
+
+def test_draw_convex_polygon():
+    polygon = coxeter.shapes.ConvexPolygon([[0, 0], [1, 0], [0, 1]])
+    scene = polygon.to_plato_scene("matplotlib", zoom=10)
+    with tempfile.NamedTemporaryFile() as tmp:
+        scene.save(tmp.name)
+
+
+def test_draw_spheropolygon():
+    spheropolygon = coxeter.shapes.ConvexSpheropolygon([[0, 0], [1, 0], [0, 1]], 0.3)
+    scene = spheropolygon.to_plato_scene("matplotlib", zoom=10)
+    with tempfile.NamedTemporaryFile() as tmp:
+        scene.save(tmp.name)
