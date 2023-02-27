a2cfd3ca839aa6527695fe98ddc4f32631f2fa20
janbridley <bradleyjenj@gmail.com>
2023-02-27 13:14:48 -0500

    Reverted small fixes that account for inaccuracies in polyhedron stored data
    Currently, the stored data for polyhedra is not accurate enough for the ``edges`` and ``edge_vectors`` properties to function as expected. Once #177 is merged, the accuracy increase should fix the issue and assertion tolerances for testing can be tightened. In addition, ``merge_faces`` should no longer be required for any of the polyhedra included with this package.

--- tests/test_polyhedron.py
+++ tests/test_polyhedron.py
@@ -236,18 +236,16 @@ def test_edges():
     }
     for name, edgelength in known_shapes.items():
         poly = PlatonicFamily.get_shape(name)
-        if name == "Dodecahedron":
-            poly.merge_faces(rtol=1)
         # Test edge_vectors property
         for edge in poly.edge_vectors:
             # rtol must be lowered to accomodate small inaccuracies in vertex locations
-            assert np.isclose(np.linalg.norm(edge), edgelength, rtol=1e-4)
+            assert np.isclose(np.linalg.norm(edge), edgelength)
 
         # Test edges property
         edges = np.asarray(poly.edges)
         vertices = poly.vertices
         veclens = np.linalg.norm(vertices[edges[:, 1]] - vertices[edges[:, 0]], axis=1)
-        assert np.allclose(veclens, edgelength, rtol=1e-4)
+        assert np.allclose(veclens, edgelength)
 
 
 def test_curvature():
