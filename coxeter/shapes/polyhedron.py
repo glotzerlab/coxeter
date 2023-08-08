8a61dd5975d159a8524c29f4758a7a2fe889503a
janbridley <bradleyjenj@gmail.com>
2023-08-08 11:36:44 -0400

    Another docstring fix

--- coxeter/shapes/polyhedron.py
+++ coxeter/shapes/polyhedron.py
@@ -87,7 +87,7 @@ class Polyhedron(Shape3D):
         >>> cube.gsd_shape_spec
         {'type': 'Mesh', 'vertices': [[-1.0, -1.0, -1.0], [-1.0, -1.0, 1.0],
         [-1.0, 1.0, -1.0], [-1.0, 1.0, 1.0], [1.0, -1.0, -1.0],
-        [1.0, -1.0, 1.0], [1.0, 1.0, -1.0], [1.0, 1.0, 1.0]], 'indices':, 'indices':
+        [1.0, -1.0, 1.0], [1.0, 1.0, -1.0], [1.0, 1.0, 1.0]], 'indices':,
         [array([0, 2, 6, 4], dtype=int32), array([0, 4, 5, 1], dtype=int32),
         array([4, 6, 7, 5], dtype=int32), array([0, 1, 3, 2], dtype=int32),
         array([2, 3, 7, 6], dtype=int32), array([1, 5, 7, 3], dtype=int32)]}
