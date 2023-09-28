44a259dfd2e7c1b56143cee0f56f594742e1d025
Domagoj Fijan <domagoj.fijan@gmail.com>
2023-09-28 18:22:18 -0400

    replace rtd with furo

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -76,7 +76,10 @@ default_role = "any"
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-html_theme = "sphinx_rtd_theme"
+html_theme = "furo"
+html_theme_options = {
+    "sidebar_hide_name": True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
