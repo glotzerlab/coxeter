e6c8a7bec6aaeebf7d87245737020c56d1a9795a
Joshua A. Anderson <joaander@umich.edu>
2024-07-08 07:21:06 -0400

    Fix line too long error.

--- coxeter/__init__.py
+++ coxeter/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2024 The Regents of the University of Michigan. All rights reserved.
+# Copyright (c) 2015-2024 The Regents of the University of Michigan.
 # This file is from the coxeter project, released under the BSD 3-Clause License.
 
 """The coxeter package simplifies working with shapes in 2D and 3D.
--- coxeter/shapes/circle.py
+++ coxeter/shapes/circle.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2015-2024 The Regents of the University of Michigan. All rights reserved.
+# Copyright (c) 2015-2024 The Regents of the University of Michigan.
 # This file is from the coxeter project, released under the BSD 3-Clause License.
 
 """Defines a circle."""
