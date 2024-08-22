b937b3aa9f855b27b74d14cf559a4d4831c3c82a
janbridley <bradleyjenj@gmail.com>
2024-08-22 12:15:33 -0400

    Update docs

--- coxeter/families/__init__.py
+++ coxeter/families/__init__.py
@@ -4,17 +4,48 @@
 """Provide tools for generating shapes.
 
 Shape families are coxeter's way of providing well-defined methods for
-generating classes of shapes according to some set of rules. The basic
-interface is defined by the :class:`~.ShapeFamily`, which is a functor that is
-called to generate a shape. The :class:`~.TabulatedShapeFamily` group of
-subclasses enable the generation of shape families according to some
-tabulated set of data, while other families are defined by some set of
-(discrete or continuous) parameters that are used to construct a shape
-analytically.
+generating classes of shapes according to some set of rules. The
+:class:`~.TabulatedGSDShapeFamily` group of subclasses enable the generation of shape
+families according to some tabulated set of data, while other families are defined by
+some set of parameters that are used to construct a shape analytically. Discrete
+families -- those that contain a fixed number of individual shapes -- may be iterated
+over.
 
-The `DOI_SHAPE_REPOSITORIES` variable provides convenient access to the shape families
-associated with different scientific publications. This dataset is useful for
+Example:
+
+    >>> from coxeter.families import PlatonicFamily
+    >>> for name, shape in PlatonicFamily:
+    >>>    print(name, shape.num_faces)
+    ... (Cube, 6)
+    ... (Dodecahedron, 12)
+    ... (Icosahedron, 20)
+    ... (Octahedron, 8)
+    ... (Tetrahedron, 4)
+
+    >>> cube = PlatonicFamily.get_shape("Cube")
+    >>> cube.num_faces
+    ... 6
+
+
+For continuous families of shapes, one must instead provide the required parameters.
+
+
+Example:
+
+    >>> from coxeter.families import Family423
+    >>> cube = Family423.get_shape(a=1.0, c=3.0) # These values yield a Platonic cube
+    >>> cube.num_faces
+    ... 6
+
+The `DOI_SHAPE_REPOSITORIES` variable provides convenient access to the shape family or
+families associated with different scientific publications. This dataset is useful for
 reproducing the exact set of shapes from publications.
+
+Example:
+
+    >>> from coxeter.families import DOI_SHAPE_REPOSITORIES
+    >>> DOI_SHAPE_REPOSITORIES["10.1103/PhysRevX.4.011024"]
+    ... [Family323Plus, Family423, Family523],
 """
 
 from .common import (
